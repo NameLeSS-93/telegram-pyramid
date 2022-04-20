473ea9d5f71ed5ebb2af4b0d0fdf0c64c214acd7
Nikita Myakishev <30523448+NameLeSS-93@users.noreply.github.com>
2022-04-21 00:37:03 +0300

    style fixing (#2)

--- utils.py
+++ utils.py
@@ -6,12 +6,10 @@ from models import Code
 def generate_user_codes(user):
     codes = []
     for _ in range(10):
-        code_str = str(
-            uuid.uuid4()
-        ).upper().replace('-', '')[:10]
+        code_str = str(uuid.uuid4()).upper().replace("-", "")[:10]
         code = Code(
             {
-                'code': code_str,
+                "code": code_str,
             }
         )
         codes.append(code_str)
